6f1e053b4c5f1d35490857b3f8a9ce3d186a45e2
kuballa <jziomek669@gmail.com>
2025-04-12 23:43:03 +0200

    add context menu

---
+++ chrome_extension/background.js
@@ -0,0 +1,39 @@
+// TEST
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "add-youtube-bookmark",
+    title: "dupa",
+    contexts: ["page"],
+    documentUrlPatterns: ["*://*.youtube.com/watch*"]
+  });
+});
+// TEST
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
+
+// TEST
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "add-youtube-bookmark" 
+    && tab.url 
+    // && tab.url.includes("youtube.com/watch")
+  ) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    
+    chrome.tabs.sendMessage(tab.id, {
+      type: "CONTEXT_ADD_BOOKMARK",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
+// TEST
