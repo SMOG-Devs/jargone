65394604fe04f5620ddf44388096cfd5fa2bbdc2
YgLK <kuba21316@wp.pl>
2025-04-12 16:49:15 +0200

    feat: init data schema files

---
+++ server/data/__init__.py
@@ -0,0 +1,6 @@
+"""
+Data is stored in a vector database and a relational database.
+
+The vector database is used to store the embeddings of the documents.
+The relational database is used to store the definitions of the named entities (knowledge base).
+"""
