5d0a3785f6854bd401e3827013138d8e44d652eb
YgLK <kuba21316@wp.pl>
2025-04-12 16:45:57 +0200

    feat: init packages

---
+++ server/ner/__init__.py
@@ -0,0 +1,14 @@
+"""
+NER (Named Entity Recognition) is a technique that uses a large language model (LLM) to identify and extract named entities from a given text.
+
+The NER process typically involves the following steps:
+
+1. **Input**:
+   - The input text.
+
+2. **Processing**:
+   - The SpaCy library uses the input text to identify and extract named entities.
+
+3. **Output**:
+   - The named entities are returned to the user with its definitions from the knowledge base.
+"""
